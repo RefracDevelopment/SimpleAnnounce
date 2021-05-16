27c46cd1fe2a264e821482a5e1268fe083f926dc
Zachary Baldwin <31602472+Refrac@users.noreply.github.com>
2021-05-16 18:30:26 -0500

    accidentally replaced all "config" with "instance.getConfig()"

--- src/main/java/me/refrac/simpleannounce/bungee/commands/AnnounceCommand.java
+++ src/main/java/me/refrac/simpleannounce/bungee/commands/AnnounceCommand.java
@@ -35,7 +35,7 @@ public class AnnounceCommand extends Command {
             player.sendMessage(Utils.formatComponent("&b&lSimpleAnnounce &7by Refrac"));
             player.sendMessage(new TextComponent(""));
             player.sendMessage(Utils.formatComponent("&b/announce <message> &7- Announce your messages"));
-            player.sendMessage(Utils.formatComponent("&b/announce reload &7- Reload your instance.getConfig() files"));
+            player.sendMessage(Utils.formatComponent("&b/announce reload &7- Reload your config files"));
         }
 
         if (args.length != 1) return;
